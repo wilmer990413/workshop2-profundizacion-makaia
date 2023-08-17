e7df54cde4797d4ef5c5b8f15e4d04f741689645
Wilmer Rodolfo Castro Ladino <wilmer.castro@unillanos.edu.co>
2023-08-17 16:45:07 -0500

    Se agrega animacion de flechas

--- js/actionsIndex.js
+++ js/actionsIndex.js
@@ -1,10 +1,12 @@
-document.getElementById('features').addEventListener('click', function(){hideOrNotHideModal('.modal__features')});
-document.getElementById('company').addEventListener('click', function(){hideOrNotHideModal('.modal__company')});
-function hideOrNotHideModal(modal){
-    console.log(document.querySelector(modal).style.display,'pare');
+document.getElementById('features').addEventListener('click', function(){hideOrNotHideModal('features','.modal__features')});
+document.getElementById('company').addEventListener('click', function(){hideOrNotHideModal('company','.modal__company')});
+
+function hideOrNotHideModal(id,modal){
     if(document.querySelector(modal).style.display === 'flex'){
+        document.getElementById(id).querySelector('img').src = './images/icon-arrow-down.svg';
         document.querySelector(modal).style.display = 'none';
     }else{
+        document.getElementById(id).querySelector('img').src = './images/icon-arrow-up.svg';
         document.querySelector(modal).style.display = 'flex';
     }
 }
